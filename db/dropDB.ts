d6bd5ae688f77bed98ef0740e3e2683cda29aabd
Nabil <mailboxofnabil@gmail.com>
2024-02-13 23:19:46 +0600

    corn fixed

--- db/dropDB.ts
+++ db/dropDB.ts
@@ -1,21 +1,11 @@
 import cron from "node-cron";
-import { initDb } from "@/db"
+import { sql } from "@vercel/postgres";
 
 
-async function deleteTableData () {
-    const db = await initDb();
-    const deleteQuery = `DELETE FROM lovers`;
-    db.run(deleteQuery, (err) => {
-      if (err) {
-        console.error('Error deleting data:', err.message);
-      } else {
-        console.log(`Data deleted from lovers successfully.`);
-      }
-    });
-  }
-  
-  // Schedule the deletation of DB to run every 24 hours
-  cron.schedule('0 0 */1 * *', () => {
-    deleteTableData();
-  });
-  
+cron.schedule('0 0 */1 * *', async () => {
+  try {
+      await sql`DELETE FROM lovers`
+    } catch (error) {
+      console.log(error)      
+    }
+});
--- db/index.ts
+++ db/index.ts
@@ -6,9 +6,13 @@ export async function initDb() {
     try {
         const result =await sql`CREATE TABLE IF NOT EXISTS lovers (link TEXT, email TEXT, name TEXT);`; 
         console.log("table created")
+      
         return NextResponse.json({ result }, { status: 200 });
     } catch (error) {
         return NextResponse.json({ error }, { status: 500 });
     }
+     // Schedule the deletation of DB to run every 24 hours
+
+  
 }
 
