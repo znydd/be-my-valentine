12c8dcaf22fd2b14509d0a463d22d0629a024024
Nabil <mailboxofnabil@gmail.com>
2024-02-13 15:18:21 +0600

    next config fix

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,3 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    images: {
-      remotePatterns: [
-        {
-          protocol: 'https',
-          hostname: 'i.pinimg.com/',
-          port: '',
-          pathname: '',
-        },
-      ],
-    },
-  }
+const nextConfig = {}
 export default nextConfig;
